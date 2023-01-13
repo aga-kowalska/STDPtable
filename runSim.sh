d53bb7cdbc60e862f679db0d6ca82a44907fc02e
Agnieszka Kowalska <AK2373@live.mdx.ac.uk>
2023-01-13 12:18:08 +0000

    add bash script file to run simulation and print results

---
+++ runSim.sh
@@ -0,0 +1,6 @@
+python3 STDPneurons.py
+
+echo "Presynaptic spikes:"
+python3 printPklSpikes.py pre_synaptic_stdp_Spikes.pkl
+echo "Postsynaptic spikes:"
+python3 printPklSpikes.py post_synaptic_stdp_Spikes.pkl
