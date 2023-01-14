2aa9c2f9a022d4b4ab7cc4271637b240b9514cdb
Agnieszka Kowalska <AK2373@live.mdx.ac.uk>
2023-01-14 15:56:43 +0000

    add spikeSource to the post synaptic neuron

--- STDPneurons.py
+++ STDPneurons.py
@@ -2,13 +2,15 @@
 import pyNN.nest as sim
 
 RUN_DURATION = 100
-TIME_STEP = 0.1
-INITIAL_WEIGHT = 0.2
-numberNeurons = 1
+TIME_STEP = 0.01
+INITIAL_WEIGHT = 0.01
 
-def makeDCSource():
-    clamp =  sim.DCSource(amplitude=0.8, start=20.0, stop=80.0)
-    return (clamp)
+def makeDCSource(inputWeight):
+    base = 0.7
+    increment = 0.1
+    myAmplitude = base + (increment * inputWeight)
+    clamp =  sim.DCSource(amplitude=myAmplitude, start=20.0, stop=80.0)
+    return clamp
 
 def makeSynapses(pre_synaptic_neuron, post_synaptic_neuron):
     stdp_model = sim.STDPMechanism(
@@ -26,6 +28,15 @@ def makeSynapses(pre_synaptic_neuron, post_synaptic_neuron):
     synapse = sim.Projection(pre_synaptic_neuron, post_synaptic_neuron,  fromListConnector, synapse_type=stdp_model)
     return synapse
     
+def makeSpikeSource(spikeTime, post_synaptic_neuron):
+    inputSpikeTime = [spikeTime]
+    spikeArray = {'spike_times':[inputSpikeTime]}
+    spikeGen = sim.Population(1, sim.SpikeSourceArray, spikeArray)
+    firingWt = 0.1
+    connector = [(0, 0, firingWt, TIME_STEP)]
+    fromListConnector = sim.FromListConnector(connector)
+    sim.Projection(spikeGen,  post_synaptic_neuron, fromListConnector, receptor_type='excitatory')
+
 
 #----main---
 sim.setup(timestep=TIME_STEP,min_delay=TIME_STEP, max_delay=TIME_STEP, debug=0)
@@ -36,11 +47,11 @@ pre_synaptic_neuron=sim.Population(1,sim.IF_cond_exp,cellparams={})
 
 post_synaptic_neuron.record({'spikes'})
 pre_synaptic_neuron.record({'spikes'})
-dCSource = makeDCSource()
+dCSource = makeDCSource(2)
 pre_synaptic_neuron.inject(dCSource)
 
 synapse = makeSynapses(pre_synaptic_neuron, post_synaptic_neuron)
-
+makeSpikeSource(05.0, post_synaptic_neuron)
 sim.run(RUN_DURATION)
 
 post_synaptic_neuron.write_data("post_synaptic_stdp_Spikes.pkl",'spikes')
